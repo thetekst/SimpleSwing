96e5e455f5c3a817c0d9111e051c0a117e68d1aa
Dmitry Tkachenko <thetekst@gmail.com>
2016-09-06 23:21:43 +0300

    Initial commit

---
+++ src/thetekst/app/Window.java
@@ -0,0 +1,48 @@
+package thetekst.app;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Window {
+    private JFrame frame;
+    private JButton button;
+    private boolean flag = true;
+
+    public static void main(String[] args) {
+        Window window = new Window();
+        window.go();
+    }
+
+    private void go() {
+        frame = new JFrame();
+        button = new JButton("click me");
+
+        button.addActionListener(new LabelListener());
+
+        MyDrawPanel myDrawPanel = new MyDrawPanel();
+
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.getContentPane().add(BorderLayout.SOUTH, button);
+        frame.getContentPane().add(BorderLayout.CENTER, myDrawPanel);
+        frame.setSize(300, 300);
+        frame.setVisible(true);
+    }
+
+    class LabelListener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent actionEvent) {
+            frame.repaint();
+
+            if (flag) {
+                button.setText("I'v been clicked");
+                flag = false;
+            } else {
+                button.setText("click me");
+                flag = true;
+            }
+        }
+    }
+
+}
